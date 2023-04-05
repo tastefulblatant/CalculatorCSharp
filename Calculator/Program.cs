f38f4688ff085f6bfc6385c54eb19b4e9c56e3b0
tastefulblatant <121983707+tastefulblatant@users.noreply.github.com>
2023-04-05 20:29:17 +0200

    Add files via upload

---
+++ Calculator/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Calculator
+{
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+
+
+                int i1, i2;
+
+
+                Console.WriteLine("Zadejte první číslo: ");
+                i1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+
+                Console.WriteLine("Zadejte druhé číslo: ");
+                i2 = int.Parse(Console.ReadLine());
+
+
+                Console.WriteLine();
+                Console.WriteLine("Zadejte operaci: * pro násobení, / pro dělení, + pro sčítání, - pro odčítání: ");
+                string operation = Console.ReadLine();
+
+
+                switch (operation)
+                {
+                    case "+":
+                        Console.WriteLine(i1 + i2);
+                        break;
+
+                    case "-":
+                        Console.WriteLine(i1 - i2);
+                        break;
+
+                    case "*":
+                        Console.WriteLine(i1 * i2);
+                        break;
+                    case "/":
+                        if (i2 != 0)
+                            Console.WriteLine(i1 / i2);
+                        else
+                            Console.WriteLine("Druhé číslo nesmí být nula.");
+                        break;
+
+
+                    default:
+                        Console.WriteLine("Nebyla zadána ani jedna platná operace. ");
+                        break;
+                }
+
+
+
+
+            }
+        }
+}
